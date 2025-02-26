035af8f385e0ebce65a129ee8939aff88587e9e6
Aninda Manocha <anindam@amazon.com>
2025-02-26 21:48:34 +0000

    increased accuracy tolerance

--- main.py
+++ main.py
@@ -510,7 +510,7 @@ def main():
         args.prompts = ["I believe the meaning of life is"]
     args.batch_size = len(args.prompts)
     args.max_length = args.seq_len
-    args.tol_map = "{None: (1e-5, 0.05), 1000: (1e-5, 0.03), 50: (1e-5, 0.02), 5: (1e-5, 0.02)}"
+    args.tol_map = "{None: (1e-5, 0.05), 1000: (1e-5, 0.03), 50: (1e-5, 0.03), 5: (1e-5, 0.03)}"
     
     model, tokenizer, generation_config = prepare_inference(NeuronLlamaForCausalLM, args)
     base_model, _, base_generation_config = prepare_inference(baseline_llama.NeuronLlamaForCausalLM, args)
